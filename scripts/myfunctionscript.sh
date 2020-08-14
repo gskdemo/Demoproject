58106d3c4e0a1ddbc77f4a34a960b328a69da849
kiran <devops@saicompany.com>
2020-08-14 14:27:04 -0400

    Added my functions in a script and placed in bash profile

---
+++ scripts/myfunctionscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Using functions in a script
+. /home/devops/Demoproject/scripts/myfunctions
+gb()
+{
+  echo "This is the last session of shell scripting"
+  echo "All the best Macha"
+}
