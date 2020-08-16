cdda41543f9dac9e008b6e7b8fc2a6d7e6fd5d64
kiran <devops@saicompany.com>
2020-08-16 16:59:02 -0400

    Added jar project

---
+++ DemoJarProject/src/main/java/com/demo/project/App.java
@@ -0,0 +1,13 @@
+package com.demo.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello to all Devops team members! V2.0 " );
+    }
+}
